a31b1efe28fbcd41d7431420c72cca10899b9be3
Kai Schallenberg <hello@kaischallenberg.de>
2020-06-27 23:35:24 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,18 @@
+// Channel, in dem der Bot joinen soll
+var channels = ['thejocraft_live'];
+
+
+// Name vom Bot
+var botusername = 'tjc_bot';
+
+
+// OAuth vom Bot => https://twitchapps.com/tmi/
+var botoauth = '';
+
+
+// Hier antwortet der Bot
+var streamername = "thejocraft_live";
+
+
+// Command
+var command = '!leistenmsg'
